1a949bdb1d9f0fd74200db76bda7ee75e7970b05
John <johnkellyoxford@gmail.com>
2020-07-30 22:06:15 +0100

    msg

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Arm64Base64Test
 {
     class Program
     {
-        static void Main(string[] args)
+        static unsafe void Main(string[] args)
         {
             var bytes = new byte[] { 83, 65, 66, 108, 65, 71, 119, 65, 98, 65, 66, 118, 65, 67, 65, 65, 86, 119, 66, 118, 65, 72, 73, 65, 98, 65, 66, 107, 65, 67, 69, 65, 73, 65, 66, 73, 65, 71, 85, 65, 98, 65, 66, 115, 65, 71, 56, 65, 73, 65, 66, 88, 65, 71, 56, 65, 99, 103, 66, 115, 65, 71, 81, 65, 73, 81, 65, 103, 65, 69, 103, 65, 90, 81, 66, 115, 65, 71, 119, 65, 98, 119, 65, 103, 65, 70, 99, 65, 98, 119, 66, 121, 65, 71, 119, 65, 90, 65, 65, 104, 65, 67, 65, 65, 83, 65, 66, 108, 65, 71, 119, 65, 98, 65, 66, 118, 65, 67, 65, 65, 86, 119, 66, 118, 65, 72, 73, 65, 98, 65, 66, 107, 65, 67, 69, 65, 73, 65, 66, 73, 65, 71, 85, 65, 98, 65, 66, 115, 65, 71, 56, 65, 73, 65, 66, 88, 65, 71, 56, 65, 99, 103, 66, 115, 65, 71, 81, 65, 73, 81, 65, 103, 65, 69, 103, 65, 90, 81, 66, 115, 65, 71, 119, 65, 98, 119, 65, 103, 65, 70, 99, 65, 98, 119, 66, 121, 65, 71, 119, 65, 90, 65, 65, 104, 65, 67, 65, 65, 83, 65, 66, 108, 65, 71, 119, 65, 98, 65, 66, 118, 65, 67, 65, 65, 86, 119, 66, 118, 65, 72, 73, 65, 98, 65, 66, 107, 65, 67, 69, };
             var result = new byte[1024];
@@ -21,6 +21,16 @@ namespace Arm64Base64Test
             Console.WriteLine(op);
 
             Console.WriteLine(MemoryMarshal.Cast<byte, char>(result.AsSpan(0, written)).ToString());
+
+            fixed (byte* pSrc = bytes)
+            fixed (byte* pEnd = result)
+            {
+                var src = pSrc;
+                var end = pEnd;
+                Base64.AvdSimd64Decode_VectorLookup(ref src, ref end, pSrc + 64, 64, 64, pSrc, pEnd);
+
+                Console.WriteLine(MemoryMarshal.Cast<byte, char>(result.AsSpan(0, written)).ToString());
+            }
         }
     }
 
@@ -759,7 +769,7 @@ public static class Base64
 
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static unsafe void AvdSimd64Decode_VectorLookup(ref byte* srcBytes, ref byte* destBytes, byte* srcEnd, int sourceLength, int destLength, byte* srcStart, byte* destStart)
+    public static unsafe void AvdSimd64Decode_VectorLookup(ref byte* srcBytes, ref byte* destBytes, byte* srcEnd, int sourceLength, int destLength, byte* srcStart, byte* destStart)
     {
         Console.WriteLine("AvdSimd64Decode_VectorLookup");
 
