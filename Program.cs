541d9230a87e4b5162e2f9a289bc2fbf07a9a4b9
John <johnkellyoxford@gmail.com>
2020-07-30 22:20:57 +0100

    msg

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace Arm64Base64Test
             {
                 var src = pSrc;
                 var end = pEnd;
-                Base64.AvdSimd64Decode_VectorLookup(ref src, ref end, pSrc + 128, 128, 128, pSrc, pEnd);
+                Base64.AvdSimd64Decode_VectorLookup(ref src, ref end, pSrc + 64, 128, 128, pSrc, pEnd);
 
                 Console.WriteLine(MemoryMarshal.Cast<byte, char>(result.AsSpan(0, written)).ToString());
             }
