3e6b971a9198bb3cd4495430fd874cc603ea20f4
John <johnkellyoxford@gmail.com>
2020-08-10 21:18:50 +0100

    f

--- Base64.cs
+++ Base64.cs
@@ -7,6 +7,20 @@ using System.Runtime.Intrinsics.Arm;
 using System.Runtime.Intrinsics.X86;
 using System.Diagnostics;
 
+namespace System.Diagnostics
+{
+    /// <summary>
+    /// Indicates a class, method, constructor, or struct should be not shown in a <see cref="StackTrace"/> even if the stack trace was created
+    /// inside the given type or method. This class cannot be inherited.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Struct, Inherited = false)]
+    public sealed class StackTraceHiddenAttribute : Attribute
+    {
+        public StackTraceHiddenAttribute() { }
+    }
+}
+
+
 public static class Base64
 {
     private const uint EncodingPad = '='; // '=', for padding
